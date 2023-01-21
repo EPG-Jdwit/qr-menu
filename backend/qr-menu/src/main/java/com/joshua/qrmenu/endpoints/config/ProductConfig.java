7695d474ca476da76e6e55cc1bb1e12a19674118
Joshua De Wit <Joshua.DeWit@UGent.be>
2023-01-21 10:10:14 +0100

    initial category product display

--- backend/qr-menu/src/main/java/com/joshua/qrmenu/endpoints/config/ProductConfig.java
+++ backend/qr-menu/src/main/java/com/joshua/qrmenu/endpoints/config/ProductConfig.java
@@ -7,15 +7,15 @@ public class ProductConfig {
 
     public static void populateProducts(ProductService productService) throws Exception {
 
-        productService.createNewProduct(new NewProduct("Stella 25", 2.9, "Kleine Stella van't vat"));
+        productService.createNewProduct(new NewProduct("Stella 25", 2000.9, "Kleine Stella van't vat"));
         productService.createNewProduct(new NewProduct("Fanta", 3.0, "Fanta Orange"));
-        productService.createNewProduct(new NewProduct("Vol-au-vent", 19.5, "Vol-au-vent met sla en frietjes"));
+        productService.createNewProduct(new NewProduct("Vol-au-vent", 19000.5, "Vol-au-vent met sla en frietjes"));
         productService.createNewProduct(new NewProduct("Dagsoep", 6.5, "Soep van de dag"));
         productService.createNewProduct(new NewProduct("Rodenbach 25", 3.5,"Kleine Rodenbach van't vat"));
-        productService.createNewProduct(new NewProduct("Coupe Vanille", 8.5,""));
+        productService.createNewProduct(new NewProduct("Coupe Vanille", 8000.5,""));
         productService.createNewProduct(new NewProduct("Glas witte wijn", 5.0,"Sauvignon blanc"));
         productService.createNewProduct(new NewProduct("Spaghetti Bolognaise", 12.5,""));
-        productService.createNewProduct(new NewProduct("Lasagna", 16.0,""));
+        productService.createNewProduct(new NewProduct("Lasagna", 16000.0,""));
         productService.createNewProduct(new NewProduct("Warme hapjes gemend", 11.5,"Assortiment van loempia's, kippenboutjes, kaasballetjes, garnaalballetjes, bitterballen en calamares ringen"));
     }
 }
--- backend/qr-menu/src/main/resources/static/categories.js
+++ backend/qr-menu/src/main/resources/static/categories.js
@@ -1,5 +1,6 @@
 "use strict";
 const categoriesUrl = "http://localhost:8081/categories";
+const categoryByNameURL = categoriesUrl + "/category?name=";
 
 window.addEventListener("load", () => getCategories());
 
@@ -12,14 +13,48 @@ function getCategories() {
   
       let text = "<table id='category-table'>";
       for (let x in categories) {
-        text += "<tr class='category-table-row'>";
+        text += "<tr class='category-table-row' onclick='getCategoryByName(&#34"+categories[x].name +"&#34)'>";
         text += "<td class='category-cell'>" + categories[x].name + "</td>";
         text += "</tr>";
       }
       text += "</table>";
-      document.getElementById("categories-container").innerHTML = text;
+      document.getElementById("item-list-container").innerHTML = text;
     }
   }
   xmlhttp.open("GET", categoriesUrl);
+  xmlhttp.send();
+}
+
+function getCategoryByName(name) {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    const response = JSON.parse(this.responseText);
+    if (response) {
+      const url = response["_links"]["categoryProducts"].href;
+      getCategoryProducts(url);
+    }
+  }
+  xmlhttp.open("GET", categoryByNameURL + name);
+  xmlhttp.send();
+}
+
+function getCategoryProducts(url) {
+  const xmlhttp = new XMLHttpRequest();
+  xmlhttp.onload = function() {
+    const response = JSON.parse(this.responseText);
+    if (response["_embedded"]) {
+      const categories = response["_embedded"]["productList"];
+      let text = "<table id='product-table'>";
+      for (let x in categories) {
+        text += "<tr class='product-table-row'>";
+        text += "<td class='product-name-cell'>" + categories[x].name + "</td>";
+        text += "<td class='product-price-cell'>€" + categories[x].price + "</td>";
+        text += "</tr>";
+      }
+      text += "</table>";
+      document.getElementById("item-list-container").innerHTML = text;
+    }
+  }
+  xmlhttp.open("GET", url);
   xmlhttp.send();
 }
