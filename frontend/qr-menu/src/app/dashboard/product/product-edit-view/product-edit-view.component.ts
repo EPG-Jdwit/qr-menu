8288f5a036dbf0371c613869c64374d8a05ef33e
Joshua De Wit <joshua.de.wit.1@gmail.com>
2023-03-07 18:26:25 +0100

    bugfix product update & small styling change

--- frontend/qr-menu/src/app/dashboard/product/product-edit-view/product-edit-view.component.ts
+++ frontend/qr-menu/src/app/dashboard/product/product-edit-view/product-edit-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import  {FloatLabelType } from '@angular/material/form-field';
 
@@ -16,22 +16,46 @@ export class ProductEditViewComponent {
   allergenics = new FormControl('');
   allergenicList: string[] = ['Milk', 'Egg', 'Seafood', 'Gluten', 'Nuts', 'Soja'];
 
+  productForm = new FormGroup({
+    name: new FormControl('', [
+      // Validators.minLength(5)
+    ]),
+    price: new FormControl('', [
+
+    ]),
+    description: new FormControl('', [
+
+    ])
+  })
+
   constructor(
     public dialogRef: MatDialogRef<ProductEditViewComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Product,
     private productService : ProductTableService
   ) {}
-  // product: Product;
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
   saveChanges(id: number): void {
+    this.copyChanges();
     this.productService.updateProduct(id, this.data);
   }
 
   getFloatLabelValue(): FloatLabelType {
     return this.floatLabelControl.value || 'auto';
+  }
+
+  private copyChanges(): void {
+    if (this.productForm.value.name) {
+      this.data.name = this.productForm.value.name;
+    }
+    if (this.productForm.value.price) {
+      this.data.price = +this.productForm.value.price;
+    }
+    if (this.productForm.value.name) {
+      this.data.description = this.productForm.value.description;
+    }
   }
 }
